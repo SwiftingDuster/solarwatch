e981ccdddfec1b78f554f3ac67e32e9f45bf29f1
chowsowying <66082627+chowsowying@users.noreply.github.com>
2021-11-18 21:29:43 +0800

    Add BT function

--- solarwatch.ino
+++ solarwatch.ino
@@ -2,6 +2,10 @@
 #include <Wire.h>
 #include <TinyScreen.h>
 #include <RTCZero.h>
+#include <STBLE.h> //Bluetooth
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define Serial SerialUSB
 
@@ -15,7 +19,39 @@ RTCZero rtc;
 
 int planetIndex = 0;
 
+/*Bluetooth------------------------------------------------------------------------------------------*/
+
+//Debug output adds extra flash and memory requirements!
+#ifndef BLE_DEBUG
+#define BLE_DEBUG true
+#endif
+
+#if defined (ARDUINO_ARCH_AVR)
+#define SerialMonitorInterface Serial
+#elif defined(ARDUINO_ARCH_SAMD)
+#define SerialMonitorInterface SerialUSB
+#endif
+
+uint8_t ble_rx_buffer[21];
+uint8_t ble_rx_buffer_len = 0;
+uint8_t ble_connection_state = false;
+#define PIPE_UART_OVER_BTLE_UART_TX_TX 0
+
+char* coordinates = reinterpret_cast<char*>(ble_rx_buffer);
+
+int lat1 = 0;
+int lat2 = 0;
+int lat3 = 0;
+int lon1 = 0;
+int lon2 = 0;
+int lon3 = 0;
+/*------------------------------------------------------------------------------------------*/
+
 void setup() {
+ 
+  
+  BLEsetup();
+
   // Init screen
   Wire.begin();
   screen.begin();
@@ -28,7 +64,7 @@ void setup() {
   rtc.setTime(18, 00, 00);
 
   // Init serial for debug
-  Serial.begin(115200);
+  Serial.begin(9600);
   delay(2000);
   Serial.println("Starting...");
   
@@ -55,10 +91,53 @@ void setup() {
   azimuth = astro.getAzimuth();
   Serial.println(altitude);
   Serial.println(azimuth);
+
+ 
 }
 
 void loop() {
+  btConnection(); //bluetooth
+  selectedPlanet(&planetIndex , &lat1 , &lat2 , &lat3 , &lon1 , &lon2 , &lon3);
   checkButtons(&planetIndex);
   updateDisplay(planetIndex);
   delay(300);
+  
 }
+
+void btConnection()
+{
+   aci_loop();//Process any ACI commands or events from the NRF8001- main BLE handler, must run often. Keep main loop short.
+    
+
+    if (ble_rx_buffer_len) {//Check if data is available
+      //SerialMonitorInterface.print(ble_rx_buffer_len);
+      SerialMonitorInterface.print("Phone");
+      SerialMonitorInterface.print(" : ");
+      SerialMonitorInterface.println((char*)ble_rx_buffer);
+      
+      int init_size = strlen(coordinates);
+      int arr[init_size];
+      int i = 0;
+
+      char *p = strtok(coordinates," ");
+      while( p != NULL)
+      {
+       arr[i++] = atoi(p);
+       p = strtok(NULL," ");
+      }
+
+      lat1 = arr[0];
+      lat2 = arr[1];
+      lat3 = arr[2];
+      lon1 = arr[3];
+      lon2 = arr[4];
+      lon3 = arr[5];
+
+      
+      ble_rx_buffer_len = 0;//clear afer reading
+    }
+}
+
+
+
+ 
